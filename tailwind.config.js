999abce7174e1bc4a243037116ad33fa45c09841
KabirKr <8161769+KabirKr@users.noreply.github.com>
2022-01-21 18:09:32 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      "soft-blue": "hsl(215, 51%, 70%)",
+      "dark-blue-main": "hsl(217, 54%, 11%)",
+      "dark-blue-card": "hsl(216, 50%, 16%)",
+      "dark-blue-line": "hsl(215, 32%, 27%)",
+      cyan: " hsl(178, 100%, 50%)",
+      white: " hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      sans: ["Outfit, sans-serif"],
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      bold: 600,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
